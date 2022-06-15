28cd29d6d0e561b55928f43de4680dc2880f5f67
ricky ardiansyah <rickyrichk@gmail.com>
2022-06-16 00:06:50 +0700

    add responsive tablet

--- components/FooterSub.jsx
+++ components/FooterSub.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import FooterItem from "./FooterItem";
 
-export default function FooterTitle({ children }) {
+export default function FooterSub({ children }) {
   return (
     <div className="w-2/12 gap-y-24 text-2xl font-bold mt-5">
       <h1 className="mb-5">{children}</h1>
+      <ul>
+        <FooterItem>Contact Us</FooterItem>
+        <FooterItem>Career</FooterItem>
+        <FooterItem>Blog</FooterItem>
+        <FooterItem>Press</FooterItem>
+      </ul>
     </div>
   );
 }
